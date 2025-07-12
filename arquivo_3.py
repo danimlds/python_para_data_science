30b7bd0b34129442db9c4715a6cbda71a3c7ade7
Daniel Moura <daniel.mlds@gmail.com>
2025-07-12 16:01:10 -0300

    update data science

---
+++ arquivo_3.py
@@ -0,0 +1,90 @@
+import sqlite3
+import matplotlib.pyplot as plt
+import pandas as pd
+
+con = sqlite3.connect('vendas.db')
+
+c = con.cursor()
+
+c.execute('''
+          CREATE TABLE IF NOT EXISTS vendas(
+          id INTEGER PRIMARY KEY AUTOINCREMENT,
+          vendas FLOAT NOT NULL,
+          vendedor TEXT NOT NULL,
+          cidade TEXT NOT NULL
+          )''')
+
+# vendas = float(input('Informe o valor das vendas: '))
+# vendedor = input('Informe o nome do vendedor: ')
+# cidade = input('Informe a cidade: ')
+con.execute('''
+               INSERT INTO vendas 
+               (vendas, vendedor, cidade)
+               VALUES ('2000', 'a', 'São Paulo')
+               ''')
+
+con.execute('''
+               INSERT INTO vendas 
+               (vendas, vendedor, cidade)
+               VALUES ('2000300', 'b', 'Rio de Janeiro')
+               ''')
+con.execute('''
+               INSERT INTO vendas 
+               (vendas, vendedor, cidade)
+               VALUES ('30000', 'c', 'Belo Horizonte')
+               ''')
+con.execute('''
+               INSERT INTO vendas 
+               (vendas, vendedor, cidade)
+               VALUES ('3000', 'd', 'Curitiba')
+               ''')
+
+# con.execute('''
+#             INSERT INTO vendas
+#             (vendas, vendedor, cidade)
+#             VALUES (?,?,?)
+#             ''', (vendas, vendedor, cidade))
+
+con.execute("SELECT * FROM vendas")
+vendas = c.fetchall()
+
+# for venda in vendas:
+#     print(f'''id:{vendas[0]},
+#           vendas:{vendas[1]},
+#           vendedor:{vendas[2]},
+#           cidade:{vendas[3]}
+#           ''')
+
+
+df = pd.read_sql_query('SELECT * FROM vendas', con)
+
+media_de_vendas = df[vendas].mean()
+print(media_de_vendas)
+
+print(df.describe())
+
+
+csvs = 'dados_exportados_2.csv'
+df.to_csv(csvs)
+
+plt.figure(figsize=(10, 5))
+plt.bar(df['vendas'], df['vendedor'], color='skyblue')
+plt.xlabel('vendas')
+plt.ylabel('vendedor')
+plt.title('VENDAS X VENDEDOR')
+plt.show()
+
+c.close()
+# Criar um DataFrame
+# data = {'Vendas': [2000,2000300,30000,3000],
+# 	      'Vendedor': ['a','b','c','d'],
+#         'Cidade': ['São Paulo', 'Rio de Janeiro', 'Belo Horizonte', 'Curitiba']}
+# df = pd.DataFrame(data)
+# print(df)
+
+# 1 Transfome em um banco de dados
+# 2 leitura de Dados de Arquivos
+# 3 Visualização de Dados
+# 4 seleção e Indexação
+# 5 Filtragem de Dados
+# 6.Manipulação de Dados
